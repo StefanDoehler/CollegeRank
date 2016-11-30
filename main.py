fba8b45e500fd88570bb634dfac3c18218b12c64
Stefan Doehler <stefan.doehler@yale.edu>
2016-11-30 03:34:02 -0500

    Can scrape the top 50 of Best Colleges, for name and rank

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 
 us_news_url_25 = "http://colleges.usnews.rankingsandreviews.com/best-colleges/rankings/national-universities"
 niche_url_25 = "https://colleges.niche.com/rankings/best-colleges/"
-niche_url_50 = ""
+best_colleges_url_50 = "http://www.thebestcolleges.org/rankings/top-50/"
 
 
 def get_html(url):
@@ -37,8 +37,11 @@ def scrape_us_news(url):
         schools.append(block.find('h3').find('a', href=True).string)
         ranks.append(re.search(r"#\d*", str(block)).group(0)[1:])
 
+    print "****" + "US NEWS" + "*"*20
     print schools,
+    print
     print ranks,
+    print
 
 
 def scrape_niche(url):
@@ -49,14 +52,48 @@ def scrape_niche(url):
 
     blocks = html.find_all(name="div", class_="ranking-item__body")
     schools, ranks = [], []
+    rank = 1
 
     for block in blocks:
         schools.append(block.find('h3').find('a', href=True).string)
-        ranks.append(block.find(name="div", class_="ranking-item__ordinal").string)
+        ranks.append(rank)
+        rank += 1
 
+    print "****" + "NICHE" + "*" * 25
     print schools,
     print
     print ranks,
+    print
 
 
-scrape_niche(niche_url)
+def scrape_best_colleges(url):
+    html = get_html(url)
+
+    if html is None:  # deal with bad url requests
+        return None
+
+    chunk = html.find(name="ol", class_="directory-links")
+    blocks = chunk.find_all("li")
+    schools, ranks = [], []
+    rank = 1
+
+    for block in blocks:
+        header = block.find('h4')
+
+        if header is None:
+            continue
+
+        schools.append(header.find('a', href=True).string)
+        ranks.append(rank)
+        rank += 1
+
+    print "****" + "BEST COLLEGES" + "*" * 15
+    print schools,
+    print
+    print ranks,
+    print
+
+
+#scrape_us_news(us_news_url_25)
+#scrape_niche(niche_url_25)
+scrape_best_colleges(best_colleges_url_50)
